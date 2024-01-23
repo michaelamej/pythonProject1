6936ddd4c211c0b67b277e59840e18ed077bf8e3
michaelamej <147770729+michaelamej@users.noreply.github.com>
2024-01-23 17:53:38 +0000

    1253

--- lendr/core/urls.py
+++ lendr/core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls.static import static
 
+from django.urls import path
+from core.views import index, contact
 from . import views
 from .forms import LoginForm
 
@@ -11,4 +15,4 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
