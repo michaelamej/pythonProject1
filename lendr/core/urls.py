c75c7033a364f8115c8b584ca2f44d8e04b31c55
michaelamej <147770729+michaelamej@users.noreply.github.com>
2024-01-23 18:22:41 +0000

    122

--- lendr/core/urls.py
+++ lendr/core/urls.py
@@ -10,8 +10,9 @@ from .forms import LoginForm
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
+    path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
